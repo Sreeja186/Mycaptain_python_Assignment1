31afcaf2c89f28c541a35c6da1924022960ee10c
Sreeja186 <124807038+Sreeja186@users.noreply.github.com>
2023-02-27 19:22:44 +0530

    Update circle_radius.py
    writing a Python program which accepts the radius of a circle from the user and computes area and a Python program to accept a filename from the user and print the extension of that.

--- circle_radius.py
+++ circle_radius.py
@@ -9,3 +9,6 @@ calculateArea = str(pi * r**2);
  
 #print result
 print ("The area of the circle with radius " + str(r) + " is: " + calculateArea)
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
